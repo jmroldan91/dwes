30c2916177911898e04c0cf2935bb56e4371c090
Juan Manuel Roldán Rueda <juanma091@gmail.com>
2015-10-09 19:35:15 +0200

    Update pruebas.php

--- pruebas.php
+++ pruebas.php
@@ -1,3 +1,4 @@
+
 <?php
     require_once 'classes/uploadFile.php';
 ?>
@@ -10,18 +11,11 @@
     <body>
         <?php
             if(!empty($_FILES)){
-                $file = new uploadFile($_FILES['file'],'./doc/',$_FILES['file']['size']);
-                //echo $file;
-                echo "subiendo...<br/>";
-                if($file->upload(true)){
-                    echo "Archivo subido";
-                }else{
-                    echo "error subiendo el archivo: " . $file->getError_message();
-                }
-                echo "<br/>$file";
+                uploadFile::MultiUpoad($_FILES, false);
             }else{
                 echo "<h1>No ha llegado ningin fichero</h1>" .error_get_last()['message'];
             }
         ?>
     </body>
 </html>
+
