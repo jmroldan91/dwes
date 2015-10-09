24d4bc13bc49e86f41dc8acca46ecc34c13222b3
Juan Manuel Roldán Rueda <juanma091@gmail.com>
2015-10-09 19:37:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,11 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <form name="form" action="pruebas.php" method="POST" enctype="multipart/form-data">
-            <input type="file" name="file" value="" width="30" max_size="9999"/><br/>
+        <form name="form" action="pruebas.php" method="POST" enctype="multipart/form-data" multiple>
+            <input type="file" name="file" value="" width="30"/><br/>
+            <input type="file" name="file2" value="" width="30"/><br/>
+            <input type="file" name="file3" value="" width="30"/><br/>
+            <input type="file" name="file4" value="" width="30"/><br/>
             <input type="submit" value="submit" name="go" />
         </form>
     </body>
